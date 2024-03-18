da6e22489ec93fdf2d6abb3ee981a0e65f380f63
한상일 <hansang-il@hansang-il-ui-MacBookAir.local>
2024-03-18 13:56:05 +0900

    [Add] Prisma 추가

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,8 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {cors: true});
-  await app.listen(8095);
+  await app.listen(3000);
+  // 배포 : 3000
+  // 개발 : 8095
 }
 bootstrap();
